bfde922efdb01633a350e4fcd16928c9b6cb17cf
AliceMathews <alice_mathews@hotmail.co.uk>
2019-12-04 23:29:02 +0000

    created a handleuserinput function. pairing with @oddporson

--- play.js
+++ play.js
@@ -1,9 +1,25 @@
-const { connect } = require('./client');
-
+const { connect } = require('./client'); // in client.js, module.export = { connect }. This is object destructuring.
 console.log('Connecting ...');
 connect();
 
-// This option also works:
-// const connect = require('./client');
-// console.log('Connecting ...');
-// connect();
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
+  stdin.on('data', handleUserInput);
+  
+  return stdin;
+}
+setupInput()
