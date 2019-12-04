fb3ff12d6100c6a7b807cfd8abd306edcdcd042b
AliceMathews <alice_mathews@hotmail.co.uk>
2019-12-04 22:23:40 +0000

    played with using setTimeout and setInterval to move the snake around

--- client.js
+++ client.js
@@ -9,13 +9,23 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
   conn.on('connect', () => {
     console.log('Successfully connected to game server.')
-    conn.write('Name: ALM')
+    conn.write('Name: ALM');
+    
+    // setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 500);
+    // setTimeout( () => {
+    //   conn.write('Move: left');
+    // }, 1000)
   });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
   return conn;
 };
 module.exports = { connect }
