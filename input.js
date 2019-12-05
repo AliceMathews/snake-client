aa0fd510270c91a13a58ee155f2e0923c98c2cbf
AliceMathews <alice_mathews@hotmail.co.uk>
2019-12-05 00:08:22 +0000

    passed conn to input to be able to conn.write instructions for keys. pairing with @oddporson

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection; 
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,11 +11,30 @@ const setupInput = function() {
     if (key === '\u0003') {
       process.exit();
     }
+  
+    if (key === 'w') {
+      conn.write('Move: up');
+    }
+  
+    if (key === 's') {
+      conn.write('Move: down');
+    }
+  
+    if (key === 'a') {
+      conn.write('Move: left');
+    }
+  
+    if (key === 'd') {
+      conn.write('Move: right');
+    }
   }
-
+  
   stdin.on('data', handleUserInput);
   
   return stdin;
 }
+
+
+
 
 module.exports = { setupInput }
